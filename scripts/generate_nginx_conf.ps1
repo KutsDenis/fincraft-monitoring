18a1b512bf06e085c8eae6c23fce333a5bf6126a
Den.is.Kuts <97687032+KutsDenis@users.noreply.github.com>
2024-12-02 02:47:30 +0500

    fix_proxy; исправлена генерация конфига nginx для корректной работы проксирования

--- scripts/generate_nginx_conf.ps1
+++ scripts/generate_nginx_conf.ps1
@@ -3,7 +3,7 @@ if (-Not (Test-Path $directory)) {
     New-Item -ItemType Directory -Path $directory
 }
 
-@"
+@'
 worker_processes auto;
 events {
     worker_connections 1024;
@@ -12,21 +12,31 @@ http {
     server {
         listen 80;
 
-        location /grafana/ {
-            proxy_pass http://grafana:3000/;
-            proxy_set_header Host `$host;
-        }
-
         location /prometheus/ {
-            proxy_pass http://prometheus:9090/;
-            proxy_set_header Host `$host;
+            rewrite ^/prometheus(/.*)$ $1 break;
+            proxy_pass http://prometheus:9090;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+
+            sub_filter_once off;
+            sub_filter 'href="/' 'href="/prometheus/';
+            sub_filter 'src="/' 'src="/prometheus/';
+            sub_filter 'action="/' 'action="/prometheus/';
+            proxy_set_header Accept-Encoding "";
+
+            proxy_redirect / /prometheus/;
         }
 
         auth_basic "Restricted Content";
         auth_basic_user_file /etc/nginx/.htpasswd;
     }
 }
-"@ | Out-File -FilePath .\nginx\nginx.conf -Encoding utf8
+'@ | Out-File -FilePath .\nginx\nginx.conf -Encoding utf8
 
-# Удаление BOM(желание вернуться к линуксу: [#################################----] 90%)
+# Удаление BOM(желание вернуться к линуксу: [#################################-] 98%)
 (Get-Content .\nginx\nginx.conf -Raw) | Set-Content -NoNewline .\nginx\nginx.conf
--- scripts/generate_nginx_conf.sh
+++ scripts/generate_nginx_conf.sh
@@ -2,7 +2,7 @@
 
 mkdir -p nginx
 
-cat <<EOF > nginx/nginx.conf
+cat <<'EOF' > nginx/nginx.conf
 worker_processes auto;
 events {
     worker_connections 1024;
@@ -11,14 +11,24 @@ http {
     server {
         listen 80;
 
-        location /grafana/ {
-            proxy_pass http://grafana:3000/;
-            proxy_set_header Host \$host;
-        }
-
         location /prometheus/ {
-            proxy_pass http://prometheus:9090/;
-            proxy_set_header Host \$host;
+            rewrite ^/prometheus(/.*)$ $1 break;
+            proxy_pass http://prometheus:9090;
+            proxy_set_header Host $host;
+            proxy_set_header X-Real-IP $remote_addr;
+            proxy_set_header X-Forwarded-For $proxy_add_x_forwarded_for;
+
+            proxy_http_version 1.1;
+            proxy_set_header Upgrade $http_upgrade;
+            proxy_set_header Connection "upgrade";
+
+            sub_filter_once off;
+            sub_filter 'href="/' 'href="/prometheus/';
+            sub_filter 'src="/' 'src="/prometheus/';
+            sub_filter 'action="/' 'action="/prometheus/';
+            proxy_set_header Accept-Encoding "";
+
+            proxy_redirect / /prometheus/;
         }
 
         auth_basic "Restricted Content";
