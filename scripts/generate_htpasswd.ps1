f02f368e2ba57d19f2bdf1fc57986a49a656dcab
Den.is.Kuts <97687032+KutsDenis@users.noreply.github.com>
2024-12-02 00:55:37 +0500

    fix_generate_htpasswd; исправлено получение логина/пароля из переменных окружения

--- scripts/generate_htpasswd.ps1
+++ scripts/generate_htpasswd.ps1
@@ -6,6 +6,25 @@ if (-Not (Test-Path $NGINX_DIR)) {
 $CurrentDir = (Get-Location).Path
 $HTPASSWD_FILE = "$NGINX_DIR\.htpasswd"
 
+$EnvFile = ".\.env"
+if (-Not [string]::IsNullOrEmpty([System.Environment]::GetEnvironmentVariable("PROMETHEUS_USER")) -and `
+    -Not [string]::IsNullOrEmpty([System.Environment]::GetEnvironmentVariable("PROMETHEUS_PASS"))) {
+    $PrometheusUser = [System.Environment]::GetEnvironmentVariable("PROMETHEUS_USER")
+    $PrometheusPass = [System.Environment]::GetEnvironmentVariable("PROMETHEUS_PASS")
+} elseif (Test-Path $EnvFile) {
+    Get-Content $EnvFile | ForEach-Object {
+        $parts = $_ -split "="
+        if ($parts.Length -eq 2) {
+            [System.Environment]::SetEnvironmentVariable($parts[0], $parts[1])
+        }
+    }
+    $PrometheusUser = [System.Environment]::GetEnvironmentVariable("PROMETHEUS_USER")
+    $PrometheusPass = [System.Environment]::GetEnvironmentVariable("PROMETHEUS_PASS")
+} else {
+    Write-Output "Error: PROMETHEUS_USER or PROMETHEUS_PASS is not set and .env file is missing"
+    exit 1
+}
+
 if (-Not (Test-Path $HTPASSWD_FILE)) {
-    docker run --rm -v "${CurrentDir}\${NGINX_DIR}:/workdir" httpd:alpine htpasswd -cb /workdir/.htpasswd "admin" "password"
+    docker run --rm -v "${CurrentDir}\${NGINX_DIR}:/workdir" httpd:alpine htpasswd -cb /workdir/.htpasswd $PrometheusUser $PrometheusPass
 }
--- scripts/generate_htpasswd.sh
+++ scripts/generate_htpasswd.sh
@@ -5,6 +5,19 @@ HTPASSWD_FILE="$NGINX_DIR/.htpasswd"
 
 mkdir -p "$NGINX_DIR"
 
+if [ -z "$PROMETHEUS_USER" ] || [ -z "$PROMETHEUS_PASS" ]; then
+  if [ -f ".env" ]; then
+    while IFS='=' read -r key value; do
+      if [[ $key != \#* && -n $key ]]; then
+        export "$key=$value"
+      fi
+    done < .env
+  else
+    echo "Error: PROMETHEUS_USER or PROMETHEUS_PASS is not set and .env file is missing"
+    exit 1
+  fi
+fi
+
 if [ ! -f "$HTPASSWD_FILE" ]; then
-  docker run --rm -v "$(pwd)/$NGINX_DIR:/workdir" httpd:alpine htpasswd -cb /workdir/.htpasswd admin password
+  docker run --rm -v "$(pwd)/$NGINX_DIR:/workdir" httpd:alpine htpasswd -cb /workdir/.htpasswd "$PROMETHEUS_USER" "$PROMETHEUS_PASS"
 fi
